a08803406ac7090114ee9a258b0755752b3e562e
Jiahua <jiahuazh_07@outlook.com>
2026-03-12 01:19:48 +0100

    French test examples added

--- make_sample.py
+++ make_sample.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("data/sps-corpus-1.0-2025-11-25-en/ss-corpus-en.tsv", sep="\t")
+df = pd.read_csv("./data/sps-corpus-1.0-2025-11-25-en/ss-corpus-en.tsv", sep="\t")
 
 sample = df.sample(n=16, random_state=42)
 
 sample.to_csv("./data/test_sample/sample_md.tsv", sep="\t", index=False)
+
+df = pd.read_csv("./data/sps-corpus-2.0-2025-12-05-fr/ss-corpus-fr.tsv", sep="\t")
+
+sample = df.sample(n=16, random_state=42)
+
+sample.to_csv("./data/test_sample/sample_md_fr.tsv", sep="\t", index=False)
+
